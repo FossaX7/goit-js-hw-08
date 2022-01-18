ca6ea9e339655995630befbea5380824014f6d94
Andrii Mogylnytskyi <88145575+FossaX7@users.noreply.github.com>
2022-01-18 19:32:32 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', throttle(function() {
+    player.getCurrentTime().then(function (sec) {
+        console.log('title:', sec);
+        localStorage.setItem('current-time', JSON.stringify(sec))
+    });
+}, 1000),
+);
+
+const toStart = JSON.parse(localStorage.getItem('current-time')) || 0;
+console.log(toStart);
+player.setCurrentTime(toStart);
